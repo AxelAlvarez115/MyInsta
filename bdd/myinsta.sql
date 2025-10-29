f7c50a680b461a1f601dab0343fc433bc76006e4
AxelAlvarez115 <axel.alvarez115@gmail.com>
2025-10-29 09:43:59 +0100

    Db tweacks

--- bdd/myinsta.sql
+++ bdd/myinsta.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Oct 27, 2025 at 01:59 PM
--- Server version: 8.4.3
--- PHP Version: 8.3.16
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 29 oct. 2025 à 08:38
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `myinsta`
+-- Base de données : `myinsta`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
+-- Structure de la table `comments`
 --
 
-CREATE TABLE `comments` (
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
   `id` int NOT NULL,
   `content` varchar(250) NOT NULL,
   `user_id` int NOT NULL,
@@ -37,10 +38,11 @@ CREATE TABLE `comments` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `likes`
+-- Structure de la table `likes`
 --
 
-CREATE TABLE `likes` (
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE IF NOT EXISTS `likes` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
   `photo_id` int NOT NULL
@@ -49,28 +51,49 @@ CREATE TABLE `likes` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `photos`
+-- Structure de la table `photos`
 --
 
-CREATE TABLE `photos` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE IF NOT EXISTS `photos` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `link` varchar(250) NOT NULL,
   `description` varchar(250) NOT NULL,
-  `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `photos`
+--
+
+INSERT INTO `photos` (`id`, `link`, `description`, `user_id`) VALUES
+(1, '96c4f5dbd1654394dc8d06db783c76c7.jpg', 'Firestorm', 1),
+(2, 'super-cobra-helicopters-celestial-images.jpg', 'Night', 1),
+(3, 'wallpaperflare.com_wallpaper (1).jpg', 'The best', 1),
+(4, 'wallpaperflare.com_wallpaper.jpg', 'Two', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varbinary(250) NOT NULL,
-  `bio` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `bio` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `bio`) VALUES
+(1, 'OSUOD', 0x24327924313024564176374736597162644f2e595645376559584a372e6f447a736d70724f4f534d734e324839764b736d4a51564e567a386577464b, '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
